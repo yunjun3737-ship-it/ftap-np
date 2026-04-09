b3f09a250882084232b0ce4bbd9600da6e808a4d
yunjun3737-ship-it <yunjun3737@gmail.com>
2026-04-09 20:07:44 +0900

    Update ftap.lua

--- ftap.lua
+++ ftap.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
 
--- 가장 안정적인 Proxy 적용 (현재 성공률 높음)
+-- 현재 가장 잘 되는 Proxy
 local WEBHOOK_URL = "https://webhook.lewisakura.moe/1491747688415498393/wXHLss9QvcYHhFj60W-g6aLQ8lHkLkQ3yAzXJzI337MCFnQpQR5q9v31WGB9pBS4LGQQ"
 
 local function sendLog()
